8501267843bb368a66f81d3c05b6ab69ac986d04
Andy Jiang <andyj1638@gmail.com>
2020-05-01 17:54:29 -0700

    initial commit

---
+++ src/main/java/com/example/enigmaVisual/EnigmaVisualApplication.java
@@ -0,0 +1,13 @@
+package com.example.enigmaVisual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnigmaVisualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnigmaVisualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/enigmaVisual/EnigmaVisualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.enigmaVisual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnigmaVisualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
